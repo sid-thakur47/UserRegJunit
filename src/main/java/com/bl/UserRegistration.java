5595bd0f8ff9cb5fae554cc25bec4901f4ca7bf4
sid-thakur47 <sidthakur258@gmail.com>
2020-04-20 18:42:45 +0530

    ADDED:Email validation regular expression

--- src/main/java/com/bl/UserRegistration.java
+++ src/main/java/com/bl/UserRegistration.java
@@ -6,9 +6,14 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
     Pattern NAME_PATTERN = Pattern.compile( "^[A-Z][a-z]{3,}$" );
+    Pattern EMAIL_PATTERN = Pattern.compile( "^[a-z0-9]{1,}([.|-]?[a-z0-9]+)?[@][a-z0-9]{1,}.[a-z]{2,4}(.{1}[a-z]{2})?$" );
 
     public boolean validateName(String name) {
         Matcher matcher = NAME_PATTERN.matcher( name );
+        return matcher.matches();
+    }
+    public boolean validateEmail(String email) {
+        Matcher matcher = EMAIL_PATTERN.matcher( email );
         return matcher.matches();
     }
 }
