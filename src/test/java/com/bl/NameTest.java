80e1dbbae56cb797344f2c0c39cd82a9dfa9692b
sid-thakur47 <sidthakur258@gmail.com>
2020-04-20 15:45:54 +0530

    ADDED:Functionality to test name seperatetly

---
+++ src/test/java/com/bl/NameTest.java
@@ -0,0 +1,55 @@
+package com.bl;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(value = Parameterized.class)
+public class NameTest {
+
+    public String name;
+
+    UserRegistration userRegistration = new UserRegistration();
+
+    public NameTest(String name) {
+        this.name = name;
+    }
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList( new Object[][]{
+                {"Siddhesh"},
+                {"Ramesh"},
+                {"siddhesh"},
+                {"ramesh"},
+                {"Thakur"},
+                {"Koli"},
+                {"thaku@r"},
+                {"koli@"},
+        } );
+    }
+    @Test
+    public void WhenName_IsProvide_ShouldReturnTrue() {
+        boolean correctName = userRegistration.validateName( name );
+        Assert.assertTrue( correctName );
+    }
+    @Test
+    public void WhenWrongName_IsProvided_ShouldReturnFalse() {
+        boolean wrongName = userRegistration.validateName( name );
+        Assert.assertFalse( wrongName );
+    }
+
+    @Test
+    public void WhenLastName_IsProvide_ShouldReturnTrue() {
+        boolean correctLastName = userRegistration.validateName( name );
+        Assert.assertTrue( correctLastName );
+    }
+    @Test
+    public void WhenLastName_IsProvide_ShouldReturnFalse() {
+        boolean wrongLastName = userRegistration.validateName( name );
+        Assert.assertFalse( wrongLastName );
+    }
+}
