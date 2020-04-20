3712ac673333787d1fabc7c75231867d14fd35be
sid-thakur47 <sidthakur258@gmail.com>
2020-04-20 14:14:53 +0530

    ADDED:Functionality to test name seperately

---
+++ src/test/java/com/bl/NameTest.java
@@ -0,0 +1,41 @@
+package com.bl;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(value = Parameterized.class)
+public class NameTest {
+
+    public String name;
+
+    UserRegistration userRegistration = new UserRegistration();
+
+    public NameTest(String name) {
+        this.name = name;
+    }
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList( new Object[][]{
+                {"Siddhesh"},
+                {"Siddhesh"},
+                {"Ramesh"},
+                {"siddhesh"},
+                {"ramesh"},
+        } );
+    }
+    @Test
+    public void WhenName_IsProvide_ShouldReturnTrue() {
+        boolean correctName = userRegistration.validateName( name );
+        Assert.assertTrue( correctName );
+    }
+    @Test
+    public void WhenWrongName_IsProvided_ShouldReturnFalse() {
+        boolean wrongName = userRegistration.validateName( name );
+        Assert.assertFalse( wrongName );
+    }
+}
