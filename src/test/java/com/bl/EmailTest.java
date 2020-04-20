ea9909431a8f581448f8370982a3bc263088b1a3
sid-thakur47 <sidthakur258@gmail.com>
2020-04-20 19:58:38 +0530

    ADDED:Seperate file that clears all email samples

---
+++ src/test/java/com/bl/EmailTest.java
@@ -0,0 +1,59 @@
+package com.bl;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(value = Parameterized.class)
+public class EmailTest {
+
+    public String email;
+    boolean testEmail;
+    UserRegistration userRegistration = new UserRegistration();
+
+    public EmailTest(String email, boolean testEmail) {
+        this.email = email;
+        this.testEmail = testEmail;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList( new Object[][]{
+
+                {"abc@yahoo.com", true},
+                {"abc-100@yahoo.com", true},
+                {"abc.100@yahoo.com", true},
+                {"abc111@abc.com", true},
+                {"abc-100@abc.net", true},
+                {"abc.100@abc.com.au", true},
+                {"abc@1.com", true},
+                {"abc-gmail.com", false},
+                {"abc@.com.my", false},
+                {"abc123@gmail.a", false},
+                {"abc123@.com", false},
+                {"abc123@.com.com", false},
+                {".abc@abc.com", false},
+                {"abc()*@gmail.com", false},
+                {"abc@%*.com", false},
+                {"abc..2002@gmail.com", false},
+                {"abc.@gmail.com", false},
+                {"abc@abc@gmail.com", false},
+                {"abc@gmail.com.1a", false},
+                {"abc@gmail.com.aa.au", false}
+        } );
+    }
+
+    @Test
+    public void WhenRightEmail_IsProvide_ShouldReturnTrue() {
+        boolean Email = userRegistration.validateEmail( email );
+        if (testEmail) {
+            Assert.assertTrue( Email );
+        } else {
+            Assert.assertFalse( Email );
+        }
+    }
+}
