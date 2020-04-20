6f28e5ec8c3c2f05e28f201503bf8d7811e7635f
sid-thakur47 <sidthakur258@gmail.com>
2020-04-20 18:59:39 +0530

    ADDED:Functionality to test and validate phone number

--- src/main/java/com/bl/UserRegistration.java
+++ src/main/java/com/bl/UserRegistration.java
@@ -1,5 +1,6 @@
 package com.bl;
 
+import java.awt.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -7,6 +8,7 @@ public class UserRegistration {
 
     Pattern NAME_PATTERN = Pattern.compile( "^[A-Z][a-z]{3,}$" );
     Pattern EMAIL_PATTERN = Pattern.compile( "^[a-z0-9]{1,}([.|-]?[a-z0-9]+)?[@][a-z0-9]{1,}.[a-z]{2,4}(.{1}[a-z]{2})?$" );
+    Pattern PHONE_PATTERN= Pattern.compile( "^[0-9]{2}[\\s\\s ][0-9]{10}$" );
 
     public boolean validateName(String name) {
         Matcher matcher = NAME_PATTERN.matcher( name );
@@ -15,5 +17,11 @@ public class UserRegistration {
     public boolean validateEmail(String email) {
         Matcher matcher = EMAIL_PATTERN.matcher( email );
         return matcher.matches();
+    }
+;
+    public boolean validatePhoneNumber(String phoneNumber){
+        Matcher matcher = PHONE_PATTERN.matcher( phoneNumber );
+        return matcher.matches();
+
     }
 }
